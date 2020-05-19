6e80e363d54bdf70c90218f5e15b8a4ba10c8ba4
0xff800000 <0xff800000@gmail.com>
2020-05-19 17:19:27 +0200

    decoding data on single pixel 3-channels passing

--- file2vid.py
+++ file2vid.py
@@ -1,11 +1,9 @@
 import sys
-import cv2
 import pdb
 import time
 import pdb
 import matplotlib.pyplot as plt
 import numpy as np
-from cv2 import VideoWriter, VideoWriter_fourcc
 import skvideo.io
 
 def file2array(path):
@@ -21,58 +19,57 @@ def file2vid(path,FPS,width,height):
     # Open and convert data
     data = file2array(path)
     data = np.array(data, dtype=np.uint8)
-    #data = np.unpackbits(data)
-
-    # Create video
-    #fourcc = VideoWriter_fourcc(*'MP42')
-    ##fourcc = VideoWriter_fourcc(*'X264')
-    ##fourcc = VideoWriter_fourcc(*'HEVC')
-    #video = VideoWriter('{}.mkv'.format(path), fourcc, float(FPS), (width, height))
+    data = np.unpackbits(data) * 255
+    # Black and white images
+    #data = data.repeat(3,axis=0)
 
     # Reshape data into frames
-    repeat_size = 4
-    nb_frames = int(np.ceil(data.shape[0]/(width/repeat_size*height/repeat_size*3)))
-    padding_size = (width/repeat_size*height/repeat_size*3*nb_frames) - data.shape[0]
-    padding = np.zeros(int(padding_size))
-    data = np.concatenate((data,padding))
-    data = data.reshape((nb_frames,int(height/repeat_size),int(width/repeat_size),3))
+    repeat_size = 0
+    if repeat_size:
+        nb_frames = int(np.ceil(data.shape[0]/(width/repeat_size*height/repeat_size*3)))
+        padding_size = (width/repeat_size*height/repeat_size*3*nb_frames) - data.shape[0]
+        padding = np.zeros(int(padding_size))
+        data = np.concatenate((data,padding))
+        data = data.reshape((nb_frames,int(height/repeat_size),int(width/repeat_size),3))
 
-    pdb.set_trace()
-    data = data.repeat(repeat_size,axis=1).repeat(repeat_size,axis=2)
+        data = data.repeat(repeat_size,axis=1).repeat(repeat_size,axis=2)
+    else:
+        nb_frames = int(np.ceil(data.shape[0]/(width*height*3)))
+        padding_size = (width*height*3*nb_frames) - data.shape[0]
+        padding = np.zeros(int(padding_size))
+        data = np.concatenate((data,padding))
+        data = data.reshape((nb_frames,int(height),int(width),3))
+
+
 
 #    plt.imshow(data[0])
 
+    print(data.shape)
     skvideo.io.vwrite("{}.mkv".format(path),data,
-            outputdict={
-                "-r":str(FPS)
-                }
+#            outputdict={
+#                "-r":str(FPS)
+#                }
             )
-    return data
-#    for frame in data:
-#        cv2.imshow('frame',frame)
-#        plt.imshow(frame)
-#        plt.show()
-#        time.sleep(0.5)
-#        video.write(frame)
-
-
-#    # Convert bits to frames
-#    for bit in data:
-#        frame = []
-#        if bit == 1:
-#            frame = np.ones((height, width,3), dtype=np.uint8) * 255
-#        else:
-#            frame = np.zeros((height, width,3), dtype=np.uint8)
-#        #cv2.imshow('frame',frame)
-#        #plt.imshow(frame)
-#        #plt.show()
-#        #time.sleep(0.5)
-#        video.write(frame)
-#    video.release()
+    #return data
 
 def vid2file(path):
     data = skvideo.io.vread(path)
-    return data
+    print(data.shape)
+
+    # decode
+    data = np.rint(data/255).astype("uint8")
+    data = data.reshape((-1))
+    data = np.packbits(data)
+
+    #data.tofile(path+"decoded")
+    f = open(path+"decoded","wb")
+    f.write(data)
+    f.close()
+
+    #return data
+
+def reassamble(data):
+    return
 
 FPS = 2
 width = 256
@@ -81,7 +78,12 @@ height = 144
 #height = 4
 data_i = file2vid(sys.argv[1],FPS,width,height)
 data_o = vid2file(sys.argv[1]+".mkv")
-pdb.set_trace()
+#plt.imshow(data_i[0])
+#plt.show()
+#plt.imshow(data_o[0])
+#plt.show()
+#print(np.rint(data_i[0]/255) == np.rint(data_o[0]/255))
+#pdb.set_trace()
 
 
 
