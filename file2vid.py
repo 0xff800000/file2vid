13d533a12b7679ac096049f90ab14570c5445149
0xff800000 <0xff800000@gmail.com>
2020-07-13 19:03:52 +0200

    update file2vid py

--- file2vid.py
+++ file2vid.py
@@ -24,60 +24,63 @@ def file2vid(path,FPS,width,height):
     #data = data.repeat(3,axis=0)
 
     # Reshape data into frames
-    repeat_size = 0
-    if repeat_size:
-        nb_frames = int(np.ceil(data.shape[0]/(width/repeat_size*height/repeat_size*3)))
-        padding_size = (width/repeat_size*height/repeat_size*3*nb_frames) - data.shape[0]
-        padding = np.zeros(int(padding_size))
-        data = np.concatenate((data,padding))
-        data = data.reshape((nb_frames,int(height/repeat_size),int(width/repeat_size),3))
+    repeat_size = 4
+    repeat_frames = 4
+    nb_frames = int(np.ceil(data.shape[0]/(width/repeat_size*height/repeat_size*3)))
+    padding_size = (width/repeat_size*height/repeat_size*3*nb_frames) - data.shape[0]
+    padding = np.zeros(int(padding_size))
+    data = np.concatenate((data,padding))
+    data = data.reshape((nb_frames,int(height/repeat_size),int(width/repeat_size),3))
 
-        data = data.repeat(repeat_size,axis=1).repeat(repeat_size,axis=2)
-    else:
-        nb_frames = int(np.ceil(data.shape[0]/(width*height*3)))
-        padding_size = (width*height*3*nb_frames) - data.shape[0]
-        padding = np.zeros(int(padding_size))
-        data = np.concatenate((data,padding))
-        data = data.reshape((nb_frames,int(height),int(width),3))
+    writer = skvideo.io.FFmpegWriter("{}.mkv".format(path))
+    for frame in data:
+        if repeat_size:
+            frame = frame.repeat(repeat_size,axis=0).repeat(repeat_size,axis=1)
+        for i in range(repeat_frames):
+            writer.writeFrame(frame)
 
+    writer.close()
+
+    #pdb.set_trace()
+    # Repeat frames
+    #frame_repeat = 24
+    #data = data.repeat(frame_repeat,axis=0)
 
 
 #    plt.imshow(data[0])
 
     print(data.shape)
-    skvideo.io.vwrite("{}.mkv".format(path),data,
-#            outputdict={
-#                "-r":str(FPS)
-#                }
-            )
-    #return data
 
 def vid2file(path):
-    data = skvideo.io.vread(path)
-    print(data.shape)
-
-    # decode
-    data = np.rint(data/255).astype("uint8")
-    data = data.reshape((-1))
-    data = np.packbits(data)
-
-    #data.tofile(path+"decoded")
+    repeat_size = 4
+    repeat_frames = 4
+    vidreader = skvideo.io.vreader(path)
     f = open(path+"decoded","wb")
-    f.write(data)
+    #print(data.shape)
+    frame_i = 0
+    for frame in vidreader:
+        frame_i += 1
+        if frame_i % repeat_frames != 0:
+            continue
+
+        data = frame[::repeat_size,::repeat_size,:]
+        # decode
+        data = np.rint(data/255).astype("uint8")
+        data = data.reshape((-1))
+        data = np.packbits(data)
+
+        #data.tofile(path+"decoded")
+        f.write(data)
     f.close()
 
     #return data
 
-def reassamble(data):
-    return
-
 FPS = 2
-width = 256
+width = 176
 height = 144
-#width = 4
-#height = 4
-data_i = file2vid(sys.argv[1],FPS,width,height)
-data_o = vid2file(sys.argv[1]+".mkv")
+#data_i = file2vid(sys.argv[1],FPS,width,height)
+#data_o = vid2file(sys.argv[1]+".mkv")
+data_o = vid2file(sys.argv[1])
 #plt.imshow(data_i[0])
 #plt.show()
 #plt.imshow(data_o[0])
